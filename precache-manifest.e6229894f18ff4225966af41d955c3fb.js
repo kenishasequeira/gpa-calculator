5a934f0a3995008c50c8b77b5f6fa4357cd738ca
Kenisha Sequeira <kenishasequeira2001@gmail.com>
2020-07-19 15:36:47 +0400

    Updates

---
+++ precache-manifest.e6229894f18ff4225966af41d955c3fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8343abfffb5f1c2d67c0021af4b0e9",
+    "url": "/gpa-calculator/index.html"
+  },
+  {
+    "revision": "bad818f92174199b3cc9",
+    "url": "/gpa-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f78475c2e290a7eda806",
+    "url": "/gpa-calculator/static/css/main.2fc4bd4f.chunk.css"
+  },
+  {
+    "revision": "bad818f92174199b3cc9",
+    "url": "/gpa-calculator/static/js/2.3788fb23.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gpa-calculator/static/js/2.3788fb23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78475c2e290a7eda806",
+    "url": "/gpa-calculator/static/js/main.2e1f1535.chunk.js"
+  },
+  {
+    "revision": "081b66d8af5ae88a28c7",
+    "url": "/gpa-calculator/static/js/runtime-main.4aa6ad5e.js"
+  }
+]);
